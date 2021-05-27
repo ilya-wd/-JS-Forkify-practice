4f865b9ba0b36431e5fd8b2c957b91f04285e2e5
myname <ilyanekrasovx@gmail.com>
2021-05-27 12:40:53 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -134,6 +134,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the applicaiton');
+};
+
 // Call this function at the beginning, only ONCE
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -143,5 +147,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
